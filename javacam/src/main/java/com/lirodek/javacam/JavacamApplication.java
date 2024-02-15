945df776febfa46c73cbe994888bc5525b4a1487
Lirodek <96456897+Lirodek@users.noreply.github.com>
2024-02-15 17:05:41 +0900

    java campus

---
+++ javacam/src/main/java/com/lirodek/javacam/JavacamApplication.java
@@ -0,0 +1,13 @@
+package com.lirodek.javacam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacamApplication.class, args);
+	}
+
+}
