a4124a1af562c8d39eec11e8375f6354c8c37742
Lirodek <96456897+Lirodek@users.noreply.github.com>
2024-02-15 16:29:05 +0900

    commit

---
+++ vueside/src/main/java/com/lirodek/vueside/VuesideApplication.java
@@ -0,0 +1,13 @@
+package com.lirodek.vueside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuesideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuesideApplication.class, args);
+	}
+
+}
---
+++ vueside/src/test/java/com/lirodek/vueside/VuesideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lirodek.vueside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuesideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
